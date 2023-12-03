459320ceefc245537435552e13b42bcb94f9807a
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2023-12-03 00:41:25 -0500

    I'm curious to do batching first

--- qml/layer.hpp
+++ qml/layer.hpp
@@ -1,15 +1,18 @@
 #ifndef LAYER_H
 #define LAYER_H
 #include <string.h>
+
+#define F_TY double
+
 struct Layer {
 public:
   int input_size;
   int output_size;
 
   virtual void zero_grad(){};
-  virtual void apply(double *input) {}
-  virtual double *output() { return NULL; }
-  virtual void update_input_grad(double *input, double *input_grad) {}
+  virtual void apply(F_TY *input) {}
+  virtual F_TY *output() { return NULL; }
+  virtual void update_input_grad(F_TY *input, double *input_grad) {}
   virtual double *grad_ptr() { return NULL; }
   virtual void step(double step_size) {}
 };

685fb291e2f87a97613837628c0ab6ac9b90fac2
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2023-12-04 23:19:42 -0500

    doingo kay

--- qml/main.cpp
+++ qml/main.cpp
@@ -15,7 +15,7 @@
 
 double function(double *input) {
   return 2 * input[0] / MAX(0.5, input[4]) + input[1] * input[3] -
-         input[3] * input[2] * 0.5 - 0.1 * input[4];
+         input[2] * 0.5 - 0.1 * input[3];
 }
 
 void fill_input(double *input, int input_size) {
@@ -40,18 +40,20 @@ double calc_loss(Model model, int trials) {
 void test_training() {
   Model md;
   int input_size = 5;
-  int l1_size = 100;
-  int l2_size = 100;
+  int l1_size = 1000;
+  int l2_size = 10;
   Linear *l1 = new Linear(input_size, l1_size, -1, 1, -1, 1);
-  Tanh *r1 = new Tanh(l1_size, 5, 5);
+  Tanh *r1 = new Tanh(l1_size, 20, 5);
   Linear *l2 = new Linear(l1_size, l2_size, -1, 1, -1, 1);
-  Tanh *r2 = new Tanh(l2_size, 5, 5);
+  Tanh *r2 = new Tanh(l2_size, 20, 5);
   Linear *l3 = new Linear(l2_size, 1, -1, 1, -1, 1);
   md.layers = std::vector<Layer *>{l1, r1, l2, r2, l3};
   double *input = (double *)malloc(input_size * sizeof(double));
   int num_trains = 10000;
+  int lr_decay_index = num_trains / 2;
   int num_val = 100;
-  double lr = 0.00001;
+  double lr = 0.01;
+  double decay_lr = 0.001;
   printf("Loss at start: %f\n", calc_loss(md, num_val));
   size_t c1 = 0;
   size_t c2 = 0;
@@ -60,7 +62,8 @@ void test_training() {
     fill_input(input, input_size);
     double correct_output = function(input);
     // printf("Trying with input: %f, %f\n", input[0], input[1]);
-    std::pair<int, int> t = md.train_on_input(input, &correct_output, lr);
+    std::pair<int, int> t = md.train_on_input(
+        input, &correct_output, i < lr_decay_index ? lr : decay_lr);
     c1 += t.first;
     c2 += t.second;
   }

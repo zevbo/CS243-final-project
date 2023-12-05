d7103bcf84e0030ceccf4720082d304aaf20334b
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2023-12-04 23:43:58 -0500

    added test

--- qml/test.cpp
+++ qml/test.cpp
@@ -1,8 +1,8 @@
+#include "test.hpp"
 #include "linear.hpp"
 #include "model.hpp"
 #include "relu.hpp"
 #include "tanh.hpp"
-#include "test.hpp"
 #include "utils.hpp"
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,18 +14,18 @@
 
 #define MAX(a, b) (a > b ? a : b)
 
-double function(double *input) {
+static double function(double *input) {
   return 2 * input[0] / MAX(0.5, input[4]) + input[1] * sqrt(input[3]) -
          input[2] * 0.5 - 0.1 * input[3];
 }
 
-void fill_input(double *input, int input_size) {
+static void fill_input(double *input, int input_size) {
   for (int j = 0; j < input_size; j++) {
     input[j] = rand_f();
   }
 }
 
-double calc_loss(Model model, int trials) {
+static double calc_loss(Model model, int trials) {
   int input_size = model.layers[0]->input_size;
   double *input = (double *)malloc(input_size * sizeof(double));
   double total_loss = 0;
@@ -38,7 +38,7 @@ double calc_loss(Model model, int trials) {
   return total_loss / trials;
 }
 
-void test_training() {
+void run_test() {
   Model md;
   int input_size = 5;
   int l1_size = 1000;
@@ -58,7 +58,6 @@ void test_training() {
   printf("Loss at start: %f\n", calc_loss(md, num_val));
   size_t c1 = 0;
   size_t c2 = 0;
-  size_t t1 = microtime();
   for (int i = 0; i < num_trains; i++) {
     fill_input(input, input_size);
     double correct_output = function(input);
@@ -68,42 +67,13 @@ void test_training() {
     c1 += t.first;
     c2 += t.second;
   }
-  size_t t2 = microtime();
-  printf("Loss at end: %f\n", calc_loss(md, num_val));
-  size_t t3 = microtime();
-  printf("Calc loss time: %lu\n", t3 - t2);
-  printf("Total train time: %zu microseconds. Breakdown: %zu, %zu\n", t2 - t1,
-         c1, c2);
-}
-
-#define TY int
-void stupid_benchmark() {
-  int num_numbers = 1000;
-  TY p[num_numbers];
-  for (int i = 0; i < num_numbers; i++) {
-    p[i] = (TY)(rand_f() * 100 - 50);
+  double loss_at_end = calc_loss(md, num_val);
+  double loss_cutoff = 0.1;
+  printf("Loss at end: %f. Should be less than %d.\n", loss_at_end,
+         loss_cutoff);
+  if (loss_at_end < loss_cutoff) {
+    printf("PASSED");
+  } else {
+    printf("FAILED");
   }
-  TY p2[num_numbers];
-  for (int i = 0; i < num_numbers; i++) {
-    p2[i] = p[i];
-  }
-
-  size_t t1 = microtime();
-  TY sum = 0;
-  for (int i = 0; i < num_numbers; i++) {
-    for (int j = 0; j < num_numbers; j++) {
-      sum += p[i];
-      sum += p[j];
-      sum -= p[i];
-      sum -= p[j];
-    }
-  }
-  size_t t2 = microtime();
-  printf("Total time: %lu. Sum %f\n", t2 - t1, (float)sum);
-}
-
-int main() {
-  // stupid_benchmark();
-  // test_training();
-  run_test();
 }
